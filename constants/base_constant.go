d1b202735ee180a50b4a69323d83fe1a7abb49c0
Saint Ox <aditya.kurniawan.95@gmail.com>
2022-09-09 14:32:43 +0700

    [added] base constant file

---
+++ constants/base_constant.go
@@ -0,0 +1,24 @@
+package constants
+
+const (
+	// RouteNotFound constant
+	RouteNotFound = "Route not found"
+
+	// IncorectContentType constant
+	IncorectContentType = "Incorrect content type"
+
+	// IncorectAuthorization constant
+	IncorectAuthorization = "Incorrect authorization"
+
+	// IncorectAuthorization constant
+	IncorectBody = "Incorrect body"
+
+	// FailedParseRequest is message about failed parse request body, param, query
+	FailedParseRequest = "Failed to parse request"
+
+	// ValidationError is message about error validation request
+	ValidationError = "Validation error"
+
+	// InvalidCredential is message about invalid credential (mismatch)
+	InvalidCredential = "invalid credential"
+)
